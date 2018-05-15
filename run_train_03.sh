d32d294011deee3a0dd401f9e424a972939c75aa
bnu32 <bnuwangxun@gmail.com>
2018-05-15 10:44:47 +0800

    orth init

--- run_train_03.sh
+++ run_train_03.sh
@@ -16,8 +16,8 @@ DIM_list="512 64"
 for DIM in $DIM_list;do
     l=$checkpoints/$loss/$DATA/$DIM
     mkdir $checkpoints/$loss/$DATA/$DIM
-    CUDA_VISIBLE_DEVICES=7   python train.py -data $DATA  -net vgg  -init orth  -lr 1e-5 -dim $DIM -alpha 16  -k 32   -BatchSize 64 -loss $loss  -epochs 301 -checkpoints $checkpoints -log_dir $loss/$DATA/$DIM  -save_step 50
-    Model_LIST="0 50 100 150 200 250 300"
+    CUDA_VISIBLE_DEVICES=7   python train.py -data $DATA  -net vgg  -init orth  -lr 1e-5 -dim $DIM -alpha 16  -k 32   -BatchSize 64 -loss $loss  -epochs 501 -checkpoints $checkpoints -log_dir $loss/$DATA/$DIM  -save_step 50
+    Model_LIST="0 50 100 150 200 250 300 400 500"
     for i in $Model_LIST; do
         CUDA_VISIBLE_DEVICES=7  python test.py -data $DATA -r $l/$i$r >>result/$loss/$DATA/$DIM.txt
     done
